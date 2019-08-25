af0c16ea1991449f28c8d920fc8366c7529df8a6
poncoe <poncoe4mac@gmail.com>
2019-08-25 22:13:08 +0700

    added basic DataType

---
+++ src/basic/DataTypes.kt
@@ -0,0 +1,35 @@
+package basic
+
+// Data Types atau Tipe Data
+
+fun main() {
+    val first = "Kucing Item "
+    val last = "Putih\n"
+    val valueA = 10
+    val valueB = 20
+    print(first + last)
+    print(valueA + valueB)
+}
+
+/*
+output :
+
+Kucing Item Putih
+30
+ */
+
+/*
+\t: menambah tab ke dalam teks.
+
+\n: membuat baris baru di dalam teks.
+
+\b: menghapus satu karakter sebelumnya.
+
+\r: menghapus semua karakter sebelumnya.
+
+\’: menambah karakter single quote kedalam teks.
+
+\”: menambah karakter double quote kedalam teks.
+
+\\: menambah karakter backslash kedalam teks.
+ */
