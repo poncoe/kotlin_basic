166077bf3d26a0a190b7a69d1468bc51ab3e61bf
poncoe <poncoe4mac@gmail.com>
2019-08-25 22:13:30 +0700

    added basic HelloMeonk

---
+++ src/basic/HelloMeonk.kt
@@ -0,0 +1,15 @@
+package basic
+
+fun main() {
+    val name = "Poncoe"
+
+    print("Hi, Nama Saya Adalah ")
+    println(name)
+    print(if (true) "Saya Kucing Item Putih" else "Saya Bukan Kucing Item Putih")
+}
+
+/*
+   output:
+       Hi, Nama Saya Adalah Poncoe
+        Saya Kucing Item Putih
+*/
