052e317ac99d472f96632a76d37f6d6979d330d6
poncoe <poncoe4mac@gmail.com>
2019-08-28 08:02:10 +0700

    added functional programming Function in Function

---
+++ src/functionalprogramming/FuncInsideFunc.kt
@@ -0,0 +1,15 @@
+package functionalprogramming
+
+fun sum(valueA: Int, valueB: Int, valueC: Int): Int {
+    fun validateNumber(value: Int) {
+        if (value == 0) {
+            throw IllegalArgumentException("value must be better than 0")
+        }
+    }
+
+    validateNumber(valueA)
+    validateNumber(valueB)
+    validateNumber(valueC)
+
+    return valueA + valueB
+}
