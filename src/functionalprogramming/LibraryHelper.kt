b3c786f6af7c4b91dd9a4f98a091b6c971822137
poncoe <poncoe4mac@gmail.com>
2019-08-29 08:03:52 +0700

    added functional programming Library Helper

---
+++ src/functionalprogramming/LibraryHelper.kt
@@ -0,0 +1,12 @@
+package functionalprogramming
+
+/*
+sebuah extension functions dari sebuah objek yang menggunakan lambda sebagai argumen atau yang
+disebut sebagai higher-order function.
+ */
+
+fun main(){
+    lambdaArgument()
+    println()
+    lamdaReceiver()
+}
